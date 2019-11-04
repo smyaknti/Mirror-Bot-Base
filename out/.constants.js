1ce1a5048d3dcee00742ab08ed355f50ed3b7acb
Soumyakanti Roy <smyaknti14@gmail.com>
2019-11-04 18:33:05 +0530

    Revert "Delete .constants.js"
    This reverts commit 80eeb6142de1cb76f661749a511de44a2044ab73.

---
+++ out/.constants.js
@@ -0,0 +1,22 @@
+module.exports = Object.freeze({
+    TOKEN: '986510945:AAF87ulFvzEoYvQi-Ie-aqyeGtLcKI7PsdU',
+    ARIA_SECRET: 'wellthisisawkwardnow1234567890',
+    ARIA_DOWNLOAD_LOCATION: '/mnt/d/ubuntu/app/downloads',
+    ARIA_DOWNLOAD_LOCATION_ROOT: '/',
+    ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'],
+    ARIA_FILTERED_FILENAMES: ['YIFY'],
+    GDRIVE_PARENT_DIR_ID: '1zJ05T28yoP-zFRF7tp_c7-ipqDGLeN08',
+    SUDO_USERS: [513060646, 345],
+    AUTHORIZED_CHATS: [-1001359058397],
+    STATUS_UPDATE_INTERVAL_MS: 12000,
+    DRIVE_FILE_PRIVATE: {
+        ENABLED: false,
+        EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
+    },
+    DOWNLOAD_NOTIFY_TARGET: {
+        enabled: false,
+        host: 'hostname.domain',
+        port: 80,
+        path: '/botNotify'
+    }
+});
